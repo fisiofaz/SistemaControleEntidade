a992755f22fa0bcff6ac81b2c53ceedf116e640b
Fábio André Zatta <fisiofaz8@gmail.com>
2024-07-29 11:25:40 -0300

    feito: Adicione filtros
    Adicione filtros na view para permitir pesquisa por nome, tipo e outras palavras-chave.

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -18,5 +18,11 @@ class EntitiesController < ApplicationController
     def entity_params
       params.require(:entity).permit(:name, :entity_type, :description, :cover_image)
     end
-  end
+end
+
+def index
+  @entities = Entity.where("name LIKE ?", "%#{params[:search]}%")
+                    .order(created_at: :desc)
+                    .page(params[:page])
+end
   
