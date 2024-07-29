b1671ff78bee1f565f6a3930ffa95e981b57f95e
Fábio André Zatta <fisiofaz8@gmail.com>
2024-07-29 11:13:05 -0300

    feito: entiteis_controller

---
+++ app/controllers/entities_controller.rb
@@ -0,0 +1,22 @@
+# app/controllers/entities_controller.rb
+class EntitiesController < ApplicationController
+    def new
+      @entity = Entity.new
+    end
+  
+    def create
+      @entity = Entity.new(entity_params)
+      if @entity.save
+        redirect_to @entity, notice: 'Entity was successfully created.'
+      else
+        render :new
+      end
+    end
+  
+    private
+  
+    def entity_params
+      params.require(:entity).permit(:name, :entity_type, :description, :cover_image)
+    end
+  end
+  
